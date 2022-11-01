f059fef369a1ab8d794d82737e50a30ff0539cc4
Amrutanshu Bhat <amrubht11@gmail.com>
2022-11-01 14:01:49 +0530

    Compatibility for the statements in xls sheets

--- Paytm_Money_P&L Visualizer.py
+++ Paytm_Money_P&L Visualizer.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[27]:
 
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
 #to increase the plot area
-plt.rcParams["figure.figsize"] = (23,12)
+plt.rcParams["figure.figsize"] = (15,8)
 import random
 
 
-# In[3]:
+# In[28]:
 
 
-#realized P&L tab from the excel to be exported as CSV file.
-mydata = pd.read_csv('oct22.csv')
-capital_used = 1000
+#Inport csv / excel file - Rename the file to 'plstatement'
+try:
+    mydata = pd.read_csv('plstatement.csv')
+except FileNotFoundError:
+    mydata = pd.read_excel('plstatement.xls',sheet_name=1)
+except:
+    print("Something went wrong")
+capital_used = 200000
 df = pd.DataFrame(mydata)
 header=[]
 #Data Cleaning - Remove NaN values and junk data.
 for index,row in df.iterrows():
-    if row[0] =="Scrip Name":
+    if 'Realized P&L' in row.to_list():
         header = row
         break
     df.drop(index,axis=0,inplace=True)
@@ -30,7 +36,7 @@ df = df.set_axis(header,axis='columns')
 df.dropna(axis=1,how="all",inplace=True)
 
 
-# In[4]:
+# In[29]:
 
 
 #data analyzer - calculate profit / loss made on a single trading day.
@@ -48,30 +54,43 @@ for key,val in profit_by_date.items():
     number_of_days+=1
 
 
-# In[5]:
+# In[30]:
 
 
 #plot the graph
 fig,ax = plt.subplots()
 x_values = profit_by_date.keys()
 y_values = profit_by_date.values()
-left, width = 20, .5
-bottom, height = 15, .5
 
 bar_color = ['#007500' if v>0 else '#ff0000' for v in y_values]
 y_pos = np.arange(len(x_values))
-hbars = ax.barh(y_pos, y_values, align="center", color=bar_color)
+hbars = ax.bar(y_pos, y_values, align="center", color=bar_color)
 
-ax.set_yticks(y_pos, labels=x_values, fontsize=15)
-ax.invert_yaxis()
-ax.set_title('P&L Summary for the month of XXXXXX 20XX',fontsize=23, weight='heavy')
-ax.text(left,bottom,"Total Profit: %.2f"%(final_profit), fontsize = 20,ha="right")
-ax.text(left,bottom+0.75,"Average Profits per traded days: {:.2f}".format(final_profit/number_of_days), fontsize = 20,ha="right")
-if capital_used>0:
-    ax.text(left,bottom+1.5,"ROI: {:.2f}%".format((final_profit*100)/capital_used), fontsize = 20,ha="right")
-ax.bar_label(hbars,fmt="%.1f",label_type='edge',padding=5,fontsize=13, weight='bold')
+ax.set_xticks(y_pos, labels=x_values, fontsize=8,rotation=90)
+
+ax.set_title('P&L Summary for the month of XXX',fontsize=18, weight='heavy')
+ax.bar_label(hbars,fmt="%.1f",label_type='edge',padding=5,fontsize=10, weight='bold')
+
+
+props = dict(boxstyle='round', facecolor='#7EC8E3', alpha=0.5)
+textstr = '\n'.join((
+    'Total Profit=%.2f' % (final_profit),
+    'Avg. Profit per Day=%.2f' % ((final_profit/number_of_days)),
+    "ROI: {:.2f}%".format((final_profit*100)/capital_used)))
+
+
+# place a text box in upper left in axes coords
+ax.text(0.25, 0.15, textstr, transform=ax.transAxes, fontsize=14,
+        verticalalignment='top', bbox=props)
+
 
 plt.show()
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
